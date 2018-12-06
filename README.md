739f0d2caafa244de322b44b8ea6ed3b0c4107d4
Wade Cosby <wadecosby@wades-mbp.attlocal.net>
2018-12-06 08:00:03 -0600

    First pages commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# huntsalesmemorial
---
+++ client/precache-manifest.b5936823870ba3418bc153bedd511b43.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d155cbe66cb04f3d39ef",
+    "url": "/static/js/main.d155cbe6.chunk.js"
+  },
+  {
+    "revision": "13249856db92fcf3fe7c",
+    "url": "/static/js/1.13249856.chunk.js"
+  },
+  {
+    "revision": "d155cbe66cb04f3d39ef",
+    "url": "/static/css/main.2ce5cb85.chunk.css"
+  },
+  {
+    "revision": "30807f3ce8fd29854f4ed5cff95c50cd",
+    "url": "/index.html"
+  }
+];
